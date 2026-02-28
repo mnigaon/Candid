ebcabde5e214228573cb0c003620d73f69b8581b
mnigaon <wps.gaon@gmail.com>
2026-02-28 00:33:28 -0700

    update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,7 @@ export default {
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            checks: ["none"],
         }),
     ],
 } satisfies NextAuthConfig;
